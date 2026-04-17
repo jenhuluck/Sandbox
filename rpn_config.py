b0e224c39d1a128cb89604f6a9194d7f7bd0b1d0
jhu <jenhuluck@gmail.com>
2026-04-17 12:02:57 -0400

    Update rpn_config.py

--- rpn_config.py
+++ rpn_config.py
@@ -1,339 +1,40 @@
-"""
-Configuration for DOTA pretrained SAR RPN inference and caching (ResNet101 backbone).
-Modified from pretrain_frcnn_dota_r101_sar_wavelet.py
-to output and cache RPN proposals.
-"""
+# rpn_config_msfa.py
+_base_ = '/home/jhu/SATLOCK/MSFA/MSFA/local_configs/SARDet/r50_dota_pretrain/fg_frcnn_dota_pretrain_sar_wavelet_r50.py'
 
-auto_scale_lr = dict(base_batch_size=16, enable=False)
-backend_args = None
-data_root = '/root/siton-gpfs-pubdata/remote_sense/split_ms_dota/'
-dataset_type = 'DotaDataset'
-default_hooks = dict(
-    checkpoint=dict(
-        interval=10,
-        rule='greater',
-        save_best='coco/bbox_mAP',
-        type='CheckpointHook'),
-    logger=dict(interval=1000, type='LoggerHook'),
-    param_scheduler=dict(type='ParamSchedulerHook'),
-    sampler_seed=dict(type='DistSamplerSeedHook'),
-    timer=dict(type='IterTimerHook'),
-    visualization=dict(type='DetVisualizationHook'))
-default_scope = 'mmdet'
-env_cfg = dict(
-    cudnn_benchmark=False,
-    dist_cfg=dict(backend='nccl'),
-    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0))
-launcher = 'pytorch'
-load_from = None
-log_level = 'INFO'
-log_processor = dict(by_epoch=True, type='LogProcessor', window_size=100)
+# Keep more raw proposals from the RPN.
 model = dict(
-    backbone=dict(
-        backbone=dict(
-            depth=101,
-            frozen_stages=1,
-            init_cfg=None,
-            norm_cfg=dict(requires_grad=True, type='BN'),
-            norm_eval=True,
-            num_stages=4,
-            out_indices=(
-                0,
-                1,
-                2,
-                3,
-            ),
-            style='pytorch',
-            type='ResNet'),
-        init_cfg=dict(
-            checkpoint=
-            '/root/siton-gpfs-archive/yuxuanli/mmpretrain/work_dirs/r101_sar_wavelet/epoch_100.pth',
-            prefix='backbone',
-            type='Pretrained'),
-        input_size=(
-            1024,
-            1024,
-        ),
-        type='Self_features_model',
-        use_sar=True,
-        use_wavelet=True),
-    data_preprocessor=dict(
-        bgr_to_rgb=True,
-        mean=[
-            123.675,
-            116.28,
-            103.53,
-        ],
-        pad_size_divisor=32,
-        std=[
-            58.395,
-            57.12,
-            57.375,
-        ],
-        type='DetDataPreprocessor'),
-    neck=dict(
-        in_channels=[
-            256,
-            512,
-            1024,
-            2048,
-        ],
-        num_outs=5,
-        out_channels=256,
-        type='FPN'),
-    roi_head=dict(
-        bbox_head=dict(
-            bbox_coder=dict(
-                target_means=[
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                ],
-                target_stds=[
-                    0.1,
-                    0.1,
-                    0.2,
-                    0.2,
-                ],
-                type='DeltaXYWHBBoxCoder'),
-            fc_out_channels=1024,
-            in_channels=256,
-            loss_bbox=dict(loss_weight=1.0, type='L1Loss'),
-            loss_cls=dict(
-                loss_weight=1.0, type='CrossEntropyLoss', use_sigmoid=False),
-            num_classes=15,
-            reg_class_agnostic=False,
-            roi_feat_size=7,
-            type='Shared2FCBBoxHead'),
-        bbox_roi_extractor=dict(
-            featmap_strides=[
-                4,
-                8,
-                16,
-                32,
-            ],
-            out_channels=256,
-            roi_layer=dict(output_size=7, sampling_ratio=0, type='RoIAlign'),
-            type='SingleRoIExtractor'),
-        type='StandardRoIHead'),
-    rpn_head=dict(
-        anchor_generator=dict(
-            ratios=[
-                0.5,
-                1.0,
-                2.0,
-            ],
-            scales=[
-                8,
-            ],
-            strides=[
-                4,
-                8,
-                16,
-                32,
-                64,
-            ],
-            type='AnchorGenerator'),
-        bbox_coder=dict(
-            target_means=[
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-            ],
-            target_stds=[
-                1.0,
-                1.0,
-                1.0,
-                1.0,
-            ],
-            type='DeltaXYWHBBoxCoder'),
-        feat_channels=256,
-        in_channels=256,
-        loss_bbox=dict(loss_weight=1.0, type='L1Loss'),
-        loss_cls=dict(
-            loss_weight=1.0, type='CrossEntropyLoss', use_sigmoid=True),
-        type='RPNHead'),
     test_cfg=dict(
-        rcnn=dict(
-            max_per_img=100,
-            nms=dict(iou_threshold=0.5, type='nms'),
-            score_thr=0.05),
         rpn=dict(
-            max_per_img=1000,
+            nms_pre=12000,
+            max_per_img=4000,
+            nms=dict(type='nms', iou_threshold=0.9),
             min_bbox_size=0,
-            nms=dict(iou_threshold=0.7, type='nms'),
-            nms_pre=1000)),
-    train_cfg=dict(
+        ),
+        # RCNN settings do not matter if you only save RPN proposals,
+        # but keep them loose for debugging/comparison.
         rcnn=dict(
-            assigner=dict(
-                ignore_iof_thr=-1,
-                match_low_quality=False,
-                min_pos_iou=0.5,
-                neg_iou_thr=0.5,
-                pos_iou_thr=0.5,
-                type='MaxIoUAssigner'),
-            debug=False,
-            pos_weight=-1,
-            sampler=dict(
-                add_gt_as_proposals=True,
-                neg_pos_ub=-1,
-                num=512,
-                pos_fraction=0.25,
-                type='RandomSampler')),
-        rpn=dict(
-            allowed_border=-1,
-            assigner=dict(
-                ignore_iof_thr=-1,
-                match_low_quality=True,
-                min_pos_iou=0.3,
-                neg_iou_thr=0.3,
-                pos_iou_thr=0.7,
-                type='MaxIoUAssigner'),
-            debug=False,
-            pos_weight=-1,
-            sampler=dict(
-                add_gt_as_proposals=False,
-                neg_pos_ub=-1,
-                num=256,
-                pos_fraction=0.5,
-                type='RandomSampler')),
-        rpn_proposal=dict(
+            score_thr=0.001,
+            nms=dict(type='nms', iou_threshold=0.7),
             max_per_img=1000,
-            min_bbox_size=0,
-            nms=dict(iou_threshold=0.7, type='nms'),
-            nms_pre=2000)),
-    type='FasterRCNN')
-num_class = 15
-
-# RPN-specific configuration for caching
-rpn_cache_cfg = dict(
-    max_proposals_per_image=1000,
-    score_threshold=0.0,
-    nms_threshold=0.7,
-    save_format='pickle'  # or 'npz', 'json'
+        ),
+    )
 )
 
-optim_wrapper = dict(
-    optimizer=dict(
-        betas=(
-            0.9,
-            0.999,
-        ), lr=0.0001, type='AdamW', weight_decay=0.05),
-    type='OptimWrapper')
-param_scheduler = [
-    dict(
-        begin=0, by_epoch=False, end=500, start_factor=0.001, type='LinearLR'),
-    dict(
-        begin=0,
-        by_epoch=True,
-        end=12,
-        gamma=0.1,
-        milestones=[
-            8,
-            11,
-        ],
-        type='MultiStepLR'),
-]
-resume = False
-test_cfg = None
+# Used by the script below for proposal caching behavior.
+rpn_cache_cfg = dict(
+    max_proposals_per_image=4000,
+    score_threshold=0.0,
+    nms_threshold=0.9,
+    save_format='pickle',
+)
+
+# Optional: match your larger test size if you want.
+# Remove LoadAnnotations for plain inference on Umbra.
 test_pipeline = [
-    dict(backend_args=None, type='LoadImageFromFile'),
-    dict(keep_ratio=False, scale=(
-        1024,
-        1024,
-    ), type='Resize'),
-    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='LoadImageFromFile', backend_args=None),
+    dict(type='Resize', scale=(1024, 1024), keep_ratio=False),
     dict(
-        meta_keys=(
-            'img_id',
-            'img_path',
-            'ori_shape',
-            'img_shape',
-            'scale_factor',
-        ),
-        type='PackDetInputs'),
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape', 'scale_factor'),
+    ),
 ]
-train_cfg = dict(max_epochs=12, type='EpochBasedTrainLoop', val_interval=1)
-train_dataloader = dict(
-    batch_sampler=dict(type='AspectRatioBatchSampler'),
-    batch_size=2,
-    dataset=dict(
-        ann_file='hbb_trainval/trainval.json',
-        backend_args=None,
-        data_prefix=dict(img='hbb_trainval/images'),
-        data_root='/root/siton-gpfs-pubdata/remote_sense/split_ms_dota/',
-        filter_cfg=dict(filter_empty_gt=True, min_size=16),
-        pipeline=[
-            dict(backend_args=None, type='LoadImageFromFile'),
-            dict(type='LoadAnnotations', with_bbox=True),
-            dict(keep_ratio=False, scale=(
-                1024,
-                1024,
-            ), type='Resize'),
-            dict(prob=0.5, type='RandomFlip'),
-            dict(type='PackDetInputs'),
-        ],
-        type='DotaDataset'),
-    num_workers=2,
-    persistent_workers=True,
-    sampler=dict(shuffle=True, type='DefaultSampler'))
-train_pipeline = [
-    dict(backend_args=None, type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(keep_ratio=False, scale=(
-        1024,
-        1024,
-    ), type='Resize'),
-    dict(prob=0.5, type='RandomFlip'),
-    dict(type='PackDetInputs'),
-]
-val_cfg = dict(type='ValLoop')
-val_dataloader = dict(
-    batch_size=1,
-    dataset=dict(
-        ann_file='hbb_val/val.json',
-        backend_args=None,
-        data_prefix=dict(img='hbb_val/images'),
-        data_root='/root/siton-gpfs-pubdata/remote_sense/split_ms_dota/',
-        pipeline=[
-            dict(backend_args=None, type='LoadImageFromFile'),
-            dict(keep_ratio=False, scale=(
-                1024,
-                1024,
-            ), type='Resize'),
-            dict(type='LoadAnnotations', with_bbox=True),
-            dict(
-                meta_keys=(
-                    'img_id',
-                    'img_path',
-                    'ori_shape',
-                    'img_shape',
-                    'scale_factor',
-                ),
-                type='PackDetInputs'),
-        ],
-        test_mode=True,
-        type='DotaDataset'),
-    drop_last=False,
-    num_workers=2,
-    persistent_workers=True,
-    sampler=dict(shuffle=False, type='DefaultSampler'))
-val_evaluator = dict(
-    ann_file=
-    '/root/siton-gpfs-pubdata/remote_sense/split_ms_dota/hbb_val/val.json',
-    backend_args=None,
-    format_only=False,
-    metric='bbox',
-    type='CocoMetric')
-vis_backends = [
-    dict(type='LocalVisBackend'),
-]
-visualizer = dict(
-    name='visualizer',
-    type='DetLocalVisualizer',
-    vis_backends=[
-        dict(type='LocalVisBackend'),
-    ])
-work_dir = './work_dirs/pretrain_frcnn_dota_r101_sar_wavelet_rpn_cache'
